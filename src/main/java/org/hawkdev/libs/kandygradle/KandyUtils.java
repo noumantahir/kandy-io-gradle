ba915e994cff0c22727187bdd1d886900ba78928
Nouman Tahir <noumantahir@live.com>
2017-03-18 19:00:22 +0500

    first commit for library

---
+++ src/main/java/org/hawkdev/libs/kandygradle/KandyUtils.java
@@ -0,0 +1,11 @@
+package org.hawkdev.libs.kandygradle;
+
+/**
+ * Created by nomo on 18/03/2017.
+ */
+
+public class KandyUtils {
+    public static int sumNumbers(int num1, int num2){
+        return num1 + num2;
+    }
+}
